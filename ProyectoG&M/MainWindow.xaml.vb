6ccaecd9ca6d8c67127ccdfd7d2c2d02e43fb5b4
Junior Baez <junior_baez@ucne.edu.do>
2023-08-23 11:03:02 -0400

    Creacion del Login

--- ProyectoG&M/MainWindow.xaml.vb
+++ ProyectoG&M/MainWindow.xaml.vb
@@ -1,3 +1,13 @@
 ﻿Class MainWindow
+    Private Sub IniciarSesionButton_Click(sender As Object, e As RoutedEventArgs)
 
+    End Sub
+
+    Private Sub SalirButton_Click(sender As Object, e As RoutedEventArgs)
+
+    End Sub
+
+    Private Sub ContraseñaTextBox_KeyDown(sender As Object, e As KeyEventArgs)
+
+    End Sub
 End Class
