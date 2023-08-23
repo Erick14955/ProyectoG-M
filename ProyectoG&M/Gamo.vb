c7d607abed1a28418aa1a5cc3b94b49daec9b827
Erickson Fana Morales <erickson_fana@ucne.edu.do>
2023-08-23 11:35:34 -0400

    Configurando Inicio de sesion

---
+++ ProyectoG&M/Gamo.vb
@@ -0,0 +1,48 @@
+﻿Imports System.Data
+Imports System.Data.SqlClient
+
+Module Gamo
+    Public Sub llenadt(ByVal sql As String, ByRef dt As Data.DataTable, ByVal cs As String, Optional ByVal time As Integer = 30)
+        Try
+            Dim cn As New SqlConnection
+            Dim cmd As New SqlCommand
+            Dim da As New SqlDataAdapter
+            dt = New DataTable
+            dt.Clear()
+            cn.ConnectionString = cs
+            cn.Open()
+            cmd.Connection = cn
+            cmd.CommandTimeout = time
+            cmd.CommandText = sql
+            da.SelectCommand = cmd
+            da.Fill(dt)
+            cn.Close()
+            cn.Dispose()
+            cmd.Dispose()
+        Catch ex As Exception
+            ThowException(ex, sql)
+        End Try
+    End Sub
+    Sub ThowException(ByVal Ex As Exception, Optional ByVal query As String = "")
+        Dim i As Integer
+        Dim sException, Err, sTmp As String
+        Dim aErrorLines
+        Try
+            Err = Ex.StackTrace.Replace("in ", "}").Replace("at ", "{") 'Delimitar las lineas Interesantes con {}
+            aErrorLines = Err.Split("{")
+            sException = Ex.Message & vbCr
+
+            For i = 0 To aErrorLines.Length - 1
+                sTmp = aErrorLines(i)
+                If sTmp.Contains("}") Then
+                    sException &= sTmp.Substring(0, sTmp.IndexOf("}") - 1) & " " & sTmp.Substring(sTmp.IndexOf(":line"))
+                End If
+            Next
+
+            MsgBox("Ocurrio una Excepcion :" & vbCr & sException)
+        Catch exe As Exception
+            MsgBox("Ha Ocurrido una Excepcion en ThowException : " & exe.Message)
+
+        End Try
+    End Sub
+End Module
--- ProyectoG&M/MainWindow.xaml.vb
+++ ProyectoG&M/MainWindow.xaml.vb
@@ -1,13 +1,64 @@
-﻿Class MainWindow
-    Private Sub IniciarSesionButton_Click(sender As Object, e As RoutedEventArgs)
+﻿Imports System.Data
+Imports System.Data.SqlClient
 
+Class MainWindow
+    Dim conexion As String
+    Dim dt As DataTable
+    Private Sub Window_Loaded(sender As Object, e As RoutedEventArgs) Handles MyBase.Loaded
+        conexion = My.Settings.Conexion
+        NombreTextBox.Focus()
+    End Sub
+    Private Sub IniciarSesionButton_Click(sender As Object, e As RoutedEventArgs)
+        Dim consulta As String
+
+        If String.IsNullOrEmpty(NombreTextBox.Text) Then
+            MsgBox("Debe introducir un usuario correcto", MsgBoxStyle.Exclamation, "Gamo Tour")
+            NombreTextBox.Focus()
+        Else
+            If String.IsNullOrEmpty(ContraseñaTextBox.Password) Then
+                MsgBox("Debe introducir una contraseña valida", MsgBoxStyle.Exclamation, "Gamo Tours")
+                ContraseñaTextBox.Focus()
+            End If
+        End If
+
+        consulta = "Select * from usuarios where usuario = '" & NombreTextBox.Text & "' and password = '" & ContraseñaTextBox.Password & "' and activo = 1"
+
+        If NombreTextBox.Text <> "" And ContraseñaTextBox.Password <> "" Then
+            llenadt(consulta, dt, conexion)
+
+            If dt.Rows.Count > 0 Then
+                Me.Close()
+            End If
+        End If
     End Sub
 
     Private Sub SalirButton_Click(sender As Object, e As RoutedEventArgs)
-
+        End
     End Sub
 
     Private Sub ContraseñaTextBox_KeyDown(sender As Object, e As KeyEventArgs)
+        If e.Key = Key.Enter Then
+            Dim consulta As String
 
+            If String.IsNullOrEmpty(NombreTextBox.Text) Then
+                MsgBox("Debe introducir un usuario correcto", MsgBoxStyle.Exclamation, "Gamo Tour")
+                NombreTextBox.Focus()
+            Else
+                If String.IsNullOrEmpty(ContraseñaTextBox.Password) Then
+                    MsgBox("Debe introducir una contraseña valida", MsgBoxStyle.Exclamation, "Gamo Tours")
+                    ContraseñaTextBox.Focus()
+                End If
+            End If
+
+            consulta = "Select * from usuarios where usuario = '" & NombreTextBox.Text & "' and password = '" & ContraseñaTextBox.Password & "' and activo = 1"
+
+            If NombreTextBox.Text <> "" And ContraseñaTextBox.Password <> "" Then
+                llenadt(consulta, dt, conexion)
+
+                If dt.Rows.Count > 0 Then
+                    Me.Close()
+                End If
+            End If
+        End If
     End Sub
 End Class
